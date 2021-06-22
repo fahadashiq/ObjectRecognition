50968152c93115b702cd5a6b202fcf5d2fa7ad08
Fahad Ashiq <fahadashiquworx@gmail.com>
2021-06-22 10:33:07 +0200

    updated

--- ObjectRecognition/object_detection.py
+++ ObjectRecognition/object_detection.py
@@ -1,13 +1,7 @@
-# For running inference on the TF-Hub module.
+# Main class for object detection in image
 import tensorflow as tf
 
 import tensorflow_hub as hub
-
-# For downloading the image.
-import matplotlib.pyplot as plt
-import tempfile
-from six.moves.urllib.request import urlopen
-from six import BytesIO
 
 # For drawing onto the image.
 import numpy as np
@@ -15,16 +9,10 @@ from PIL import Image
 from PIL import ImageColor
 from PIL import ImageDraw
 from PIL import ImageFont
-from PIL import ImageOps
 
 # For measuring the inference time.
 import time
 
-# Print Tensorflow version
-print(tf.__version__)
-
-# Check available GPU devices.
-print("The following GPU devices are available: %s" % tf.test.gpu_device_name())
 # module_handle = "https://tfhub.dev/google/faster_rcnn/openimages_v4/inception_resnet_v2/1"
 module_handle = "https://tfhub.dev/google/openimages_v4/ssd/mobilenet_v2/1"
 detector = hub.load(module_handle).signatures['default']
--- ObjectRecognition/views.py
+++ ObjectRecognition/views.py
@@ -3,15 +3,11 @@ from django.http import HttpResponse
 from .models import PredictedImage
 import pythoncom
 import win32com.client as wincl
-import base64
-from django.shortcuts import redirect
-from django.core.files.base import ContentFile
 from django.conf import settings
 from . import object_detection
 from PIL import Image
 import uuid
 from django.core.files.storage import FileSystemStorage
-
 
 
 # view for text to speech
@@ -41,70 +37,35 @@ def objectDetection(request):
         fs = FileSystemStorage()
         unique_filename = str(uuid.uuid4())
         full_file_path = settings.BASE_DIR + '/media/uploaded/' + unique_filename + myfile.name
+        # Save uploaded file in local directory
         filename = fs.save(full_file_path, myfile)
-        list_of_name = []
-        #ImgObj = UploadedImage(pic=myfile)
 
-
-        # image_np, list_of_classes = object_detection_custom.perdictMultiObj(ImgObj.pic.path)
+        # Get prediction results
         image_np, list_of_percentage, list_of_classes = object_detection.detect_img(filename)
-        # convert those ids into names
-        '''
-        for li in list_of_classes:
-            class_name = object_detection_custom.category_index[li]['name']
-            list_of_name.append(class_name)
-            print(class_name)
 
-        # get image from numpy array and save it in perdicted folder with a unique name
-        img = Image.fromarray(image_np, 'RGB')
-        unique_filename = str(uuid.uuid4())
-        print(unique_filename)
-        unique = settings.BASE_DIR + '/media' + '/perdicted/' + unique_filename + '.jpg'
-        img.save(unique)
-        # make a string of perdictions
-        list_2 = []
-        for li in list_of_name:
-            if li in list_2:
-                print("nohing")
-                # do nothing
-            else:
-                list_2.append(li)
-        names = ""
-        print(list_2)
-        for li in list_2:
-            if names == "":
-                names = li
-            else:
-                names = names + '-' + li
-        names = names.replace(' ', '_')
-        # save the image address in database
-
-        ImgObj.perdiction = names
-        ImgObj.derived_perdiction = 'Let me find'
-        ImgObj.save()
-        pimg = PredictedImage()
-        pimg.pic = settings.BASE_DIR + '/media' + '/perdicted/' + unique_filename + '.jpg'
-        pimg.perdiction = names
-        pimg.save()
-        print("passed")'''
+        # Save image with bounded boxes in local dir
         img = Image.fromarray(image_np, 'RGB')
         unique_id = str(uuid.uuid4())
         unique_filename_and_address = '/predicted/' + unique_id + '.jpg'
         img.save(settings.MEDIA_ROOT + unique_filename_and_address)
 
+        # Image to display on frontend
         pimg = PredictedImage()
         pimg.pic = unique_filename_and_address
 
         list_of_classes_modified = []
         list_of_percentage_modified = []
 
+        # Convert bytes array into string
         for class_name in list_of_classes:
             list_of_classes_modified.append(bytes.decode(class_name))
 
+        # Format percentages correctly
         for percentage in list_of_percentage:
             list_of_percentage_modified.append(round(percentage * 100, 2))
 
         detection = zip(list_of_classes_modified, list_of_percentage_modified);
 
         return render(request, 'objectDetection.html', {'pimg': pimg, 'detection': detection})
+
     return render(request, 'objectDetection.html')
